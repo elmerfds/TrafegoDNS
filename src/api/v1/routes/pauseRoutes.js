e1e22d0ca37670ca0029de3da0f5d024a9f14a9a
elmerfds <e.fds@outlook.com>
2025-06-01 16:10:49 +0100

    fix(logger): update logger import to use direct assignment

--- src/api/v1/routes/pauseRoutes.js
+++ src/api/v1/routes/pauseRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { authenticate } = require('../middleware/authMiddleware')
-const { logger } = require('../../../utils/logger')
+const logger = require('../../../utils/logger')
 
 /**
  * @swagger
--- src/services/PauseManager.js
+++ src/services/PauseManager.js
@@ -1,5 +1,5 @@
 const EventEmitter = require('events')
-const { logger } = require('../utils/logger')
+const logger = require('../utils/logger')
 
 class PauseManager extends EventEmitter {
   constructor() {
